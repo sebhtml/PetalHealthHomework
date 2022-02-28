8177d635783c32ea8a038a3559525124f55e6a63
Sebastien Boisvert <sebhtml@protonmail.com>
2022-02-28 06:49:32 -0500

    (#2) backend: migrade database schema to add new column

--- pokedex_backend/db/schema.rb
+++ pokedex_backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_02_27_234946) do
+ActiveRecord::Schema[7.0].define(version: 2022_02_28_004929) do
   create_table "pokemons", force: :cascade do |t|
     t.string "name"
     t.string "type1"
@@ -26,6 +26,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_02_27_234946) do
     t.boolean "legendary"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "pokemon_number"
   end
 
 end
